eff7ce07b98e63efc5273a15b3fd6f2153bd94e2
TristanPhilp <philpts@alfredstate.edu>
2026-07-23 20:36:32 -0400

    Merge pull request #8 from Caltropian/Cal-Production
    Level development into main

---
+++ -placeholder Unity project-/Assets/Scripts/Mechanics/Walk Movement.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+public class WalkMovement : MonoBehaviour
+{
+    [Header("Movement Parameters")]
+    [SerializeField]
+    private float walkSpeed;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    void FixedUpdate()
+    {
+        
+    }
+}
